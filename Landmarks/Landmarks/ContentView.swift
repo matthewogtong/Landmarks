923de02a0b5d7d1f6e8ce90e4073043bf803cb2a
Matthew Ogtong <ogtongm@gmail.com>
2023-02-13 17:12:11 -0500

    Add initial Xcode project template

---
+++ Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Matthew Ogtong on 2/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+        }
+        .padding() 
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
