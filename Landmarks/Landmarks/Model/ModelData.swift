f669df186a806230260f7217e285577fb50d2fe1
Matthew Ogtong <ogtongm@gmail.com>
2023-02-14 14:35:06 -0500

    Create LandmarkRow view

--- Landmarks/Landmarks/Model/ModelData.swift
+++ Landmarks/Landmarks/Model/ModelData.swift
@@ -10,10 +10,10 @@ import Foundation
 var landmarks: [Landmark] = load("landmarkData.json")
 
 func load<T: Decodable>(_ filename: String) -> T {
-    
     let data: Data
     
-    guard let file = Bundle.main.url(forResource: filename, withExtension: nil) else {
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+    else {
         fatalError("Couldn't find \(filename) in main bundle.")
     }
     
@@ -29,5 +29,4 @@ func load<T: Decodable>(_ filename: String) -> T {
     } catch {
         fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
     }
-    
 }
